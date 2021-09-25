538303dc234ebf1ecda33306a608117165762584
impact-eintr <yixingwei4@gmail.com>
2021-09-25 18:15:59 +0800

    k8s随机选择一个容器执行，其他算是服务资源备份

--- k8s/main.go
+++ k8s/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 
 	"github.com/gin-gonic/gin"
 )
+
+var count int32 = 0
 
 func main() {
 	fmt.Println("欢迎使用gin框架")
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
+		atomic.AddInt32(&count, 1)
+		log.Println(count)
 		c.String(http.StatusOK, "Hello, 世界")
 	})
 	r.Run()
