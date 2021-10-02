16c053eac5ecdf906e23b3e495a31ca0c0473a4f
impact-eintr <yixingwei4@gmail.com>
2021-10-02 08:45:26 +0800

    存活指针

--- k8s/main.go
+++ k8s/main.go
@@ -2,25 +2,26 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("欢迎使用gin框架")
-	s := []string{}
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		s = append(s, "h")
-		if len(s) > 1024 {
-			log.Println("more than 1024")
-		}
-		c.String(http.StatusOK, s[0])
+		c.String(http.StatusOK, "hello gin")
 	})
+	started := time.Now()
 	r.GET("/healthz", func(c *gin.Context) {
-		c.JSON(http.StatusOK, nil)
+		duration := time.Now().Sub(started)
+		if duration.Seconds() < 20 {
+			c.JSON(http.StatusServiceUnavailable, nil)
+		} else {
+			c.JSON(http.StatusOK, nil)
+		}
 	})
 	r.Run()
 }
